33c001891359eca75e75e3fd8f63ac918d739615
Валерия <valeria103020@gmail.com>
2020-09-30 17:01:22 +0300

    itog

---
+++ vvp lab 4 №2/vvp lab 4 №2.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	double d, L;
+	const double pi = 3.14;
+	printf("Enter d: ");
+	scanf_s("%lf", &d);
+	L = d * pi;
+	printf("%lf", L);
+	return 0;
+}
